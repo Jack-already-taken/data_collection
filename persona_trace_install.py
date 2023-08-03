3a6ff935514b82513468ef0ed5c9e5cb80a256b9
Jack-already-taken <52902295+Jack-already-taken@users.noreply.github.com>
2023-08-03 12:46:40 -0700

    Delete persona_trace_install.py

--- persona_trace_install.py
+++
@@ -1,8 +0,0 @@
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-import torch
-
-#save the model to a local folder
-tokenizer = GPT2Tokenizer.from_pretrained("af1tang/personaGPT")
-model = GPT2LMHeadModel.from_pretrained("af1tang/personaGPT")
-tokenizer.save_pretrained("./persona")
-model.save_pretrained("./persona")
