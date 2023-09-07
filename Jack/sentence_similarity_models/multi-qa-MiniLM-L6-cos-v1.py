59d4ce00fe097c2ab113fe553ec483b65d549f50
Jack-already-taken <52902295+Jack-already-taken@users.noreply.github.com>
2023-09-07 15:14:48 -0700

    Add files via upload

---
+++ Jack/sentence_similarity_models/multi-qa-MiniLM-L6-cos-v1.py
@@ -0,0 +1,31 @@
+"""
+This model is based off of the microsoft/MiniLM-L6-H384-uncased model
+https://huggingface.co/sentence-transformers/multi-qa-MiniLM-L6-cos-v1
+"""
+
+import sys
+from sentence_transformers import SentenceTransformer, util
+
+def main(argv):
+
+	if len(sys.argv) < 3:
+		print("Error: Missing argument - test dataset")
+		exit(0)
+	
+	inf_len = 100
+
+	test_dir = sys.argv[2]
+	fs = open(test_dir + "sentences.txt")
+	sentence_list = []
+	for i in range(inf_len):
+		sentences = [next(fs).rstrip() for _ in range(100)]
+		sentence_list.append(sentences)
+	model = SentenceTransformer('sentence-transformers/multi-qa-MiniLM-L6-cos-v1')
+	
+	for batch in sentence_list:
+		embeddings = model.encode(batch)
+		cosine_scores = util.cos_sim(embeddings, embeddings)
+		#print(cosine_scores)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
---
+++ Jack/sentence_similarity_models/sentence-t5-large.py
@@ -0,0 +1,30 @@
+"""
+This model is based on st5-large-1
+https://huggingface.co/sentence-transformers/sentence-t5-large
+"""
+import sys
+from sentence_transformers import SentenceTransformer, util
+
+def main(argv):
+
+	if len(sys.argv) < 3:
+		print("Error: Missing argument - test dataset")
+		exit(0)
+	
+	inf_len = 100
+
+	test_dir = sys.argv[2]
+	fs = open(test_dir + "sentences.txt")
+	sentence_list = []
+	for i in range(inf_len):
+		sentences = [next(fs).rstrip() for _ in range(100)]
+		sentence_list.append(sentences)
+	model = SentenceTransformer('sentence-transformers/sentence-t5-large')
+	
+	for batch in sentence_list:
+		embeddings = model.encode(batch)
+		cosine_scores = util.cos_sim(embeddings, embeddings)
+		#print(cosine_scores)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
